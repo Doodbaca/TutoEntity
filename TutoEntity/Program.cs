921f325c634754f4f58cb698a0223eb363bfd8e6
Doodbaca <math.bienvenu@gmail.com>
2021-01-26 15:04:41 -0500

    Test 2

--- TutoEntity/Program.cs
+++ TutoEntity/Program.cs
@@ -6,7 +6,17 @@ namespace TutoEntity
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TypeVaccin pfizer = new TypeVaccin { Nom = "Pfizer"}; 
+            TypeVaccin moderna = new TypeVaccin { Nom = "Moderna"}; 
+            Vaccin dose1Mylene = new Vaccin { Date = DateTime.Today,NAMPatient = "LAPM12345678",Type = moderna}; 
+            Vaccin dose1Gaston = new Vaccin { Date = new DateTime(2021, 01, 15),NAMPatient = "BHEG12345678",Type = pfizer}; 
+            VaccinContext context = new VaccinContext(); 
+            context.Vaccins.Add(dose1Mylene);
+            context.Vaccins.Add(dose1Gaston);
+            context.SaveChanges(); context.Remove(dose1Gaston);
+            dose1Mylene.Type = pfizer; 
+            context.SaveChanges();
+            foreach (Vaccin vaccin in context.Vaccins) Console.WriteLine(vaccin);
         }
     }
 }
---
+++ TutoEntity/VaccinContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace TutoEntity
+{
+    class VaccinContext :DbContext
+    {
+        public DbSet<Vaccin> Vaccins { get; set; }
+        public DbSet<TypeVaccin> TypesVaccin { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Data Source = deptinfo420; Initial Catalog = VaccinMathieu; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    }
+}
